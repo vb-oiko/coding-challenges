5a0a3d3c28c2af920b911a8aa934c0d918766cc1
Vasyl Boyko <vasyl.boyko@bolt.eu>
2023-11-13 22:20:42 +0200

    add TreeNode

--- utils/index.js
+++ utils/index.js
@@ -73,4 +73,93 @@ class ListNode {
   }
 }
 
-module.exports = { expect, ListNode };
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+
+  static fromArray(arr) {
+    if (arr.length === 0) {
+      return null;
+    }
+
+    const head = new TreeNode(arr[0]);
+    let i = 1;
+
+    function traverse(level) {
+      const nodes = [];
+      for (const node of level) {
+        if (i < arr.length) {
+          node.left = new TreeNode(arr[i], null);
+          nodes.push(node.left);
+          i++;
+        }
+
+        if (i < arr.length) {
+          node.right = new TreeNode(arr[i], null);
+          nodes.push(node.right);
+          i++;
+        }
+      }
+
+      return nodes;
+    }
+
+    let level = [head];
+
+    while (i < arr.length) {
+      level = traverse(level);
+    }
+
+    return head;
+  }
+
+  static toArray(node) {
+    let result = [];
+
+    function traverse(level) {
+      let nodes = [];
+      for (const node of level) {
+        if (node) {
+          result.push(node.val);
+          nodes.push(node.left);
+          nodes.push(node.right);
+        }
+      }
+      return nodes;
+    }
+
+    let level = [node];
+
+    while (level.length) {
+      level = traverse(level);
+    }
+
+    return result;
+  }
+
+  static depth(node) {
+    let max = 0;
+
+    function traverse(node, level) {
+      if (!node) {
+        return;
+      }
+
+      if (level > max) {
+        max = level;
+      }
+
+      traverse(node.left, level + 1);
+      traverse(node.right, level + 1);
+    }
+
+    traverse(node, 1);
+
+    return max;
+  }
+}
+
+module.exports = { expect, ListNode, TreeNode };
