e193a1bd02bee9a8f4e2b393d8d07716821db331
Vasyl Boyko <vasyl.boyko@bolt.eu>
2023-11-18 12:01:10 +0200

    add nested array expect equal

--- utils/expect.js
+++ utils/expect.js
@@ -2,17 +2,7 @@ const expect = {
   array(source) {
     return {
       toEqual(target) {
-        if (source.length !== target.length) {
-          return false;
-        }
-
-        for (let i = 0; i < source.length; i++) {
-          if (source[i] !== target[i]) {
-            return false;
-          }
-        }
-
-        return true;
+        return isArrayEqual(source, target);
       },
 
       toHaveMembers(target) {
@@ -31,6 +21,32 @@ const expect = {
     };
   },
 };
+
+function isArrayEqual(source, target) {
+  if (source.length !== target.length) {
+    return false;
+  }
+
+  for (let i = 0; i < source.length; i++) {
+    if (!isEqual(source[i], target[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isEqual(a, b) {
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return isArrayEqual(a, b);
+  }
+
+  if (typeof a === typeof b) {
+    return a === b;
+  }
+
+  return false;
+}
 
 module.exports = {
   expect,
